9b8fe37813329d209c3fbd02e6140a457c4d0714
LeandroLino <leao.lino7@gmail.com>
2023-05-14 21:57:26 -0300

    FIX :: added log to see env

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2')
 const utils = require('./utils')
 
 function connect () {
+  console.log(process.env.DATABASE_URL)
   const connection = mysql.createConnection(process.env.DATABASE_URL)
   console.log('Connected to PlanetScale!')
   return connection
